76c364aed3e2fff17c003e042911d6c1f3ffaa6c
juanvel4000 <juanvel400@proton.me>
2025-02-11 19:18:10 -0500

    Several Improvements

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
+
 # upk-ng
-UPK PacKage Manager - Next Generation
+
+**u**nnamed **p**ac**k**age manager **-** **n**ext **g**eneration, a simple package manager written in python
+
+
+## features
+
+- **databases**: uses sqlite3
+- **packages**: compressed with .tar.xz, lossless and better packages
+- **simple**: built with simplicity in mind
+- **python**: built in python
+
+
+## building
+upk-ng **buildUtil** is a simple script written for easy compilation/packaging of the **upk-ng** package manager
+make sure you have the dependencies to use **buildUtil**
+
+### dependencies
+- **nuitka**: required for compiling the package, install with `pip`
+- **python**: required for nuitka and the package itself
+- **bash**: for running buildUtil
+- **ccache**, **distcc**: optional for speeding the compilation times
+
+### compilation
+1. compile using buildUtil
+
+~~~bash
+./buildUtil.sh build <version> <release> <maintainer>
+~~~
+**tip**: you can put your own version, release and maintainer, probably you already noticed it
+
+_this will compile **upk-ng** to the **output/** directory_
+
+2. package the build
+you should put in `version` and `maintainer` the same you put in the first step
+~~~bash
+./buildUtil.sh package <version> <maintainer>
+~~~
+
+3. bootstrap the package
+install it to your system, or to a different root using the compiled upk from **output/**, as root
+~~~
+./output/upk install-local ./upk-ng-<version>.upk <root>
+~~~
+**<root> is optional**
--- src/upk.py
+++ src/upk.py
@@ -1,33 +1,45 @@
-import upk_data,upk_pkg,upk_utils,sys 
+import upk_data,upk_pkg,upk_utils,sys,os
 from upk_utils import echo
+from upk_info import version, rel, maintainer
 if __name__ == "__main__":
-    rel = 1
-    ver = "0.1"
-    if len(sys.argv) < 1:
-        echo("error: usage: << upk [command] >>")
-        sys.exit(1)
-    match sys.argv[1]:
-        case 'help':
-            echo("upk-ng")
-            echo("next generation upk package manager")
-            echo("release {rel}, version {ver}")
-        case 'remove':
-            root = "/"
-            if len(sys.argv) > 3:
-                root = sys.argv[3]
-            if len(sys.argv) < 2:
-                echo("error: missing arguments for << upk remove >>")
-                sys.exit(1)
-            upk_pkg.deletePackage(sys.argv[2], root)
-        case 'install-local':
-            root = "/"
-            if len(sys.argv) > 3:
-                root = sys.argv[3]
-            if len(sys.argv) < 2:
-                echo("error: missing arguments for << upk install-local >>")
-                sys.exit(1)
-            upk_pkg.installPackage(sys.argv[2], root)
-
-        case _:
-            echo(f"invalid command: {sys.argv[1]}, view << upk help >> for more information")
-        
+    try:
+            
+        if len(sys.argv) < 2:
+            echo("error: usage: << upk [command] >>")
+            sys.exit(1)
+        match sys.argv[1]:
+            case 'help':
+                echo(f"upk-ng {version}-{rel} (by {maintainer})")
+                echo("next generation upk package manager")
+            case 'remove':
+                root = "/"
+                if len(sys.argv) > 3:
+                    root = sys.argv[3]
+                if len(sys.argv) < 2:
+                    echo("error: missing arguments for << upk remove >>")
+                    sys.exit(1)
+                upk_pkg.deletePackage(sys.argv[2], root)
+            case 'install-local':
+                root = "/"
+                if len(sys.argv) > 3:
+                    root = sys.argv[3]
+                if len(sys.argv) < 2:
+                    echo("error: missing arguments for << upk install-local >>")
+                    sys.exit(1)
+                upk_pkg.installPackage(sys.argv[2], root)
+            case 'build':
+                if len(sys.argv) < 3:
+                    echo("error: missing arguments for << upk build >>")
+                    sys.exit(1)
+                o = None
+                if len(sys.argv) > 3:
+                    o = sys.argv[3]
+                upk_utils.compress(sys.argv[2], o)  
+            case 'version':
+                echo(f"upk-ng ver {version}")
+                echo(f"release {rel} by {maintainer}")      
+            case _:
+                echo(f"invalid command {sys.argv[1]}, view << upk help >> for more information")
+    except Exception as e:
+            echo(e)
+            sys.exit(1)
